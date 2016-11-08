ae161511b69d8790ffb6cae9af477dbc8629a980
richelbilderbeek <richel@richelbilderbeek.nl>
2016-11-08 13:13:28 +0100

    Use -DQT_WIDGETS_LIB instead

--- do_oclint.sh
+++ do_oclint.sh
@@ -9,7 +9,7 @@ h_files=`ls *.h | egrep -v "^ui_.*\.h$"`
   $h_files \
   -- \
   -c -std=c++14 -fPIE \
-  -DQT_TESTLIB_LIB \
+  -DQT_WIDGETS_LIB \
   -I/usr/include/c++/5 \
   -I/usr/include/x86_64-linux-gnu/c++/5 \
   -I/usr/include/qt5 \
